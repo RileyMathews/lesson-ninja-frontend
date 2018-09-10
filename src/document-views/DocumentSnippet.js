8041214e090e2278453a138e2b02496f6a0f70ac
Riley Mathews <rileymathews80@gmail.com>
2018-09-10 15:37:08 -0500

    added download tag to the documents snippet

--- src/document-views/DocumentSnippet.js
+++ src/document-views/DocumentSnippet.js
@@ -11,6 +11,8 @@ class DocumentSnippet extends Component {
                     <h1>document</h1>
                     <p>{this.props.document.name}</p>
                     <a href={this.props.document.s3_url} target="blank">view</a>
+                    <span>   </span>
+                    <a href={this.props.document.s3_url} download>download</a>
                 </div>
             </React.Fragment>
         )

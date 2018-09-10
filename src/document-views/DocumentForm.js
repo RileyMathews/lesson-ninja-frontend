b014cbbaf090863ed2dc5abfae4d54f0e9d4bec4
Riley Mathews <rileymathews80@gmail.com>
2018-09-10 09:00:42 -0500

    added files for document management

---
+++ src/document-views/DocumentForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class DocumentForm extends Component {
+
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default DocumentForm
---
+++ src/document-views/DocumentList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class DocumentList extends Component {
+
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default DocumentList

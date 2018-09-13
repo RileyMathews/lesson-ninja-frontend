19555c7a204945b7d2003da0b0bd594fb5f966b2
Riley Mathews <rileymathews80@gmail.com>
2018-09-13 13:58:05 -0500

    decomposed lesson detail dropdown items

--- src/lessons/DocumentLesson.js
+++ src/lessons/DocumentLesson.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import DocumentLessonSnippet from './DocumentLessonSnippet';
 import { Input } from 'bloomer/lib/elements/Form/Input';
 import "./DocumentLesson.css"
+import DropdownBox from '../display-components/DropdownBox';
 
 class DocumentLesson extends Component {
 
@@ -20,14 +20,18 @@ class DocumentLesson extends Component {
                 <Input type="text" placeholder="documents" value={this.state.filterString} onChange={this.updateSearch} />
                 <div id="lesson_document_container">
                     {this.props.documents.sort((x, y) => { return this.props.isDocInLesson(y, this.props.lesson) - this.props.isDocInLesson(x, this.props.lesson) }).filter(document => document.name.toLowerCase().includes(this.state.filterString.toLowerCase())).map(document => (
-                        <DocumentLessonSnippet
+                        <DropdownBox
                             key={document.id}
-                            document={document}
-                            addDocumentToLesson={this.props.addDocumentToLesson}
-                            removeDocumentFromLesson={this.props.removeDocumentFromLesson}
-                            docOnLesson={this.props.isDocInLesson(document, this.props.lesson)}
-                            lesson={this.props.lesson}
+                            text={`${document.name}`}
+                            callback1={() => {
+                                this.props.isDocInLesson(document, this.props.lesson) ?
+                                    this.props.removeDocumentFromLesson(this.props.lesson.id, document.id, this.props.lesson.url)
+                                    :
+                                    this.props.addDocumentToLesson(document.url, this.props.lesson.url)
+                            }}
+                            callback1Text={this.props.isDocInLesson(document, this.props.lesson) ? "remove" : "add"}
                         />
+
                     ))}
                 </div>
             </React.Fragment>

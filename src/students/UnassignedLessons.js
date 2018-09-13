ba741bbfb0d6fd6982989b02ffda55998956de19
Riley Mathews <rileymathews80@gmail.com>
2018-09-13 10:02:25 -0500

    added search functionality to lessons on student page

--- src/students/UnassignedLessons.js
+++ src/students/UnassignedLessons.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
 import UnassignedLessonSnippet from './UnassignedLessonSnippet';
+import {Input} from 'bloomer'
 
 
 class UnassignedLessons extends Component {
+
+    state = {
+        searchString: ""
+    }
+
+    updateSearch = (evt) => {
+        this.setState({searchString: evt.target.value})
+    }
 
 
     render() {
         return (
             <React.Fragment>
                 <h1>lessons</h1>
-                {this.props.lessons.filter(lesson => !this.props.isStudentOnLesson(lesson, this.props.student)).map(lesson => (
+                <Input type="text" placeholder="search lessons" value={this.state.searchString} onChange={this.updateSearch} />
+                {this.props.lessons.filter(lesson => !this.props.isStudentOnLesson(lesson, this.props.student)).filter(lesson => lesson.name.toLowerCase().includes(this.state.searchString.toLowerCase())).map(lesson => (
                     <UnassignedLessonSnippet
                         key={lesson.id}
                         lesson={lesson}
